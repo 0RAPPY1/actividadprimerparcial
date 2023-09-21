0ca86e4d5d32280d305335bed7131c79ba05d953
0RAPPY1 <145036513+0RAPPY1@users.noreply.github.com>
2023-09-21 09:14:28 -0600

    cod_olimpiadas1.py

---
+++ cod_olimpiadas1.py
@@ -0,0 +1,7 @@
+def convercion():
+  print("cuantos kilometros recorriste: ")
+  x = float(input()) #sus kilometros
+  y = x * 1000
+  print(" ")
+  print("Este es el total de metros:",y)
+convercion()
