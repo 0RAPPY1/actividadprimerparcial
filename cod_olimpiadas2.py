08751892b201db03211b8ad56304932b4e9d311d
0RAPPY1 <145036513+0RAPPY1@users.noreply.github.com>
2023-09-21 09:15:02 -0600

    cod_olimpiadas2.py

---
+++ cod_olimpiadas2.py
@@ -0,0 +1,10 @@
+def area_trangulo():
+  print("Te pedire la base y altura del triangulo")
+  print("para darte el area")
+  print(" ")
+  x = float(input("Pon tu base:")) #base
+  u = float(input("Pon tu altura:")) #Altura 
+  print(" ")
+  o = (x*u)/2
+  print("Esta es la area:",o)
+area_trangulo()
